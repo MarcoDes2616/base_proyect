2b9eef65b25dcac6506ec6f4db4559a760cad2a8
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-04-24 20:29:40 -0400

    first comit

---
+++ src/context/MainContext.jsx
@@ -0,0 +1,16 @@
+import React, { createContext, useState } from "react";
+
+const MainContext = createContext();
+
+const MainContextProvider = ({ children }) => {
+  
+  const functions = {
+  };
+
+  return (
+    <MainContext.Provider value={functions}>{children}</MainContext.Provider>
+  );
+};
+
+export { MainContextProvider };
+export default MainContext;
